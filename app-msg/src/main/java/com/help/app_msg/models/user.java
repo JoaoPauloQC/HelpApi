728f13b51b832d7a4de1a7cfeb7e9048d14a08c0
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-06-15 16:19:19 -0300

    Model user

--- app-msg/src/main/java/com/help/app_msg/models/user.java
+++ app-msg/src/main/java/com/help/app_msg/models/user.java
@@ -1,4 +1,32 @@
 package com.help.app_msg.models;
 
 public class user {
+
+    private String nome;
+    private int idade;
+    private int turma;
+    private int prontuario;
+
+    public user(String nome, int idade, int turma, int prontuario){
+        this.nome = nome;
+        this.prontuario = prontuario;
+        this.turma = turma;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public int getProntuario() {
+        return prontuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTurma() {
+        return turma;
+    }
 }
